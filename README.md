273070cfa6e43a9ccfccf1f934686fdf6253f526
Varun.G <79745296+varunG-27@users.noreply.github.com>
2022-07-14 10:58:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Corporate-Classifieds ( SpringBoot and Angular)
+
+## Quickr and OLX for your corporate company.
+### - Make and engage in offers. Gain points for each engagement you get.
+___________________________________________________
+## Steps to run the application
+- add the sqlfiles/requiredTables.sql to the mysql workbench and run it.
+- change the sql username and password in application.properties to configure according to your sql database.
+- run each microeservice.
+- ng serve to run the angular application
+
+## To check rest api for each microservice
+- **auth** - localhost:8080/authapp/swagger-ui.html
+- ** points** - localhost:8090/points/swagger-ui.html
+- **employee** - localhost:8070/swagger-ui.html
+- **offer** - localhost:8000/swagger-ui.html
+- **classified portal** - http://localhost:4200
+
+____________________________________________________
+## Screenshots
+<br/>
+<img width="1440" alt="Screenshot 2022-06-25 at 1 48 18 AM" src="https://user-images.githubusercontent.com/30109806/175664799-6f778cfa-d447-4c41-9ecc-bb0de16797f0.png">
+<br/>
+<img width="1437" alt="Screenshot 2022-06-25 at 1 48 42 AM" src="https://user-images.githubusercontent.com/30109806/175664917-73ff703c-b765-4d3e-b365-15afd9e90b8a.png">
+<br/>
+<img width="1439" alt="Screenshot 2022-06-25 at 1 50 51 AM" src="https://user-images.githubusercontent.com/30109806/175664952-2cfdf703-bfa3-4dc0-aa85-c89459e7319c.png">
+</br>
+<img width="1440" alt="Screenshot 2022-06-25 at 1 57 00 AM" src="https://user-images.githubusercontent.com/30109806/175665168-73496fee-f2b7-4c5a-a378-f6c2269dc423.png">
+</br>
+<img width="1440" alt="Screenshot 2022-06-25 at 2 00 13 AM" src="https://user-images.githubusercontent.com/30109806/175665191-eb7b2d69-f66c-4e6f-a764-15ed9bbe50af.png">
+<img width="1440" alt="Screenshot 2022-06-25 at 1 58 49 AM" src="https://user-images.githubusercontent.com/30109806/175665194-c3f613c2-0ce9-4a94-97e8-ffea20298644.png">
+<img width="1439" alt="Screenshot 2022-06-25 at 1 58 11 AM" src="https://user-images.githubusercontent.com/30109806/175665196-4189ce2d-7ff6-408b-94d0-f0df17e67b25.png">
+<img width="1440" alt="Screenshot 2022-06-25 at 1 57 41 AM" src="https://user-images.githubusercontent.com/30109806/175665201-d1bd939e-82cb-4a13-8cf7-5ff86c459e15.png">
+
+
+
+
+
+## Online version coming soon!!
---
+++ employee-microservice/target/classes/data.sql
@@ -0,0 +1,64 @@
+drop table if exists liked_by;
+drop table if exists offer;
+drop table if exists employee;
+
+create table Employee(
+	id int primary key auto_increment,
+    name varchar(50) not null,
+    department varchar(50),
+    gender varchar(6) not null,
+    age int not null,
+    contact_number bigint,
+    email varchar(100) unique,
+    points_gained int default 0,
+    check (gender in ('male','female','other')),
+    check ( age > 0 )
+    );
+    
+create table Offer(
+	id int primary key auto_increment,
+    name varchar(50) not null,
+    description varchar(100),
+    category varchar(50) not null,
+    open_date timestamp default now(),
+    closed_date timestamp,
+    engaged_date timestamp,
+    engaged_emp_id int,
+    emp_id int not null,
+    likes int default 0,
+    foreign key(engaged_emp_id) references Employee(id) on delete cascade,
+    foreign key(emp_id) references Employee(id) on delete cascade
+    );
+
+create table liked_by(
+	emp_id int,
+    offer_id int,
+    liked_on timestamp default now(),
+    primary key(emp_id,offer_id),
+    foreign key(emp_id) references Employee(id) on delete cascade,
+    foreign key(offer_id) references Offer(id) on delete cascade
+    );
+    
+    
+insert into Employee(id,name,gender,age) values(1,'Hariprasad','male',21);
+insert into Employee(id,name,gender,age) values(2,'Varun','male',21);
+insert into Employee(id,name,gender,age) values(3,'Rajesh','male',22);
+insert into Employee(id,name,gender,age) values(4,'Shruthy','female',22);
+
+insert into Offer(id,name,category,emp_id) values(1,'Offer 1' ,'Electronics',3);
+insert into Offer(id,name,category,emp_id) values(2,'Offer 2' ,'Electronics1',3);
+insert into Offer(id,name,category,emp_id) values(3,'Offer 4' ,'Electronics2',2);
+insert into Offer(id,name,category,emp_id) values(4,'Offer 5' ,'Electronics3',1);
+insert into Offer(id,name,category,emp_id) values(5,'Offer 6' ,'Electronics4',2);
+insert into Offer(id,name,category,emp_id) values(6,'Offer 3' ,'Electronics5',1);
+insert into Offer(id,name,category,emp_id) values(7,'Offer 7' ,'Electronics6',4);
+insert into Offer(id,name,category,emp_id) values(8,'Offer 8' ,'Electronics7',4);
+
+insert into liked_by(emp_id,offer_id) values(1,1);
+insert into liked_by(emp_id,offer_id) values(1,2);
+insert into liked_by(emp_id,offer_id) values(2,3);
+insert into liked_by(emp_id,offer_id) values(2,4);
+insert into liked_by(emp_id,offer_id) values(3,5);
+insert into liked_by(emp_id,offer_id) values(3,6);
+insert into liked_by(emp_id,offer_id) values(4,7);
+insert into liked_by(emp_id,offer_id) values(4,8);
